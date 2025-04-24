b115076ae9fb28bf95dadba65a4ec84b416fcf1a
Christopher Ramroop <78105128+KillerKroc23@users.noreply.github.com>
2025-04-24 16:16:47 -0400

    Updated base Controller with comments

--- laravel-12.x/app/Http/Controllers/Controller.php
+++ laravel-12.x/app/Http/Controllers/Controller.php
@@ -2,7 +2,11 @@
 
 namespace App\Http\Controllers;
 
+
+
+
 abstract class Controller
 {
-    //
+    //this is the base controller from which other controllers in your application should extend.
+    //it contains common functionality that can be shared across all controllers.
 }
