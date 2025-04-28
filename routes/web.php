35abc2fe869fb3992430bf31cad2422f4bd4ea2e
Christopher Ramroop <78105128+KillerKroc23@users.noreply.github.com>
2025-04-28 00:39:02 -0400

    All steps completed

---
+++ routes/web.php
@@ -0,0 +1,16 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\PostController;
+use App\Http\Controllers\CommentController;
+
+Route::get('/', fn() => redirect()->route('posts.index'));
+
+Route::get('/posts', [PostController::class, 'index'])
+     ->name('posts.index');
+
+Route::get('/posts/{id}', [PostController::class, 'show'])
+     ->name('posts.show');
+
+Route::post('/posts/{id}/comments', [CommentController::class, 'store'])
+     ->name('comments.store');
